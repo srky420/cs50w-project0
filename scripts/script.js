ae1fb1d94ead0ed653eef1c5b636352ba3d12d5f
srky <srky420@gmail.com>
2023-05-05 20:29:12 +0500

    Create image search styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,15 @@
-// Get apps list and apps btn
+// Apps tab
 const appsList = document.querySelector('.apps-list');
 const appsBtn = document.querySelector('.apps-btn');
 
-// Add event listener
 appsBtn.addEventListener('click', function(e) {
     appsList.classList.toggle('hidden');
+});
+
+// Settings tab
+const settingsList = document.querySelector('.settings-list');
+const settingsBtn = document.querySelector('.settings-link');
+
+settingsBtn.addEventListener('click', function(e) {
+    settingsList.classList.toggle('hidden');
 });
